c784ba2426f05574e134b3fd8ff4b7476e69e169
aditya <adisahu444777@gmail.com>
2026-01-27 01:26:42 +0530

    sidebar logo not write src

--- Frontent/src/Sidebar.jsx
+++ Frontent/src/Sidebar.jsx
@@ -71,7 +71,7 @@ function Sidebar() {
     return(
         <section className="sidebar">
             <button  onClick={createNewChat}>
-            <img src="src/assets/gpt logo.jpg" alt="gpt logo" className="logo"></img>
+            <img src="/assets/gpt-logo.jpg" alt="gpt logo" className="logo"></img>
             <span><i className="fa-solid fa-pen-to-square"></i></span>
             </button>
 
